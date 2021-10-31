eab04bc4b6bb2d4cfccb56cd4d8a9e1247aacf2e
dhsj8405 <dhsj8405@naver.com>
2021-10-31 22:26:45 +0900

    카테고리 sql작성중

--- sql_script/jblogscript.sql
+++ sql_script/jblogscript.sql
@@ -1,14 +1,35 @@
 desc blog;
 delete from user;
 delete from blog;
+delete  from user where id = 'doolly@gmai.com';
+delete  from blog where id = 'doolly@gmai.com';
+delete from category where blog_id = 'doolly@gmai.com';
+
 
 
 select * from user;
 select * from blog;
 select * from category;
-delete  from user where id = 'doolly@gmai.com';
-delete  from blog where id = 'doolly@gmai.com';
-delete from category where id = 'doolly@gmai.com';
+select * from post;
+
+select no,name, blog_Id, `desc`,post_count
+from category
+ where blog_id = 'dhsj8405'
+ and category.no = post.category_no;
+
+select * from post,category
+where post.category_no = category.no;
+
+
+select * from post;
+select count(*) from post
+				where post.category_no = '1';                
+
+insert into post values(null, '미분류포스트타이틀1' ,'미분류포스트본문1', now(),'1');
+insert into post values(null, 'post2title' ,'post2contents', now(),'4');
+
+insert into category values(null, '카테고리1', '본문1','dhsj8405') ;
+
 update user set  id = 'ehdgus8405' where name = '둘리';
 update blog set  id = 'ehdgus8405' where id = 'doolly@gmai.com';
 update blog set title = 'test02타이틀' ,logo ='teset02로고' where id = 'doolly@gmai.com';
@@ -23,7 +44,7 @@ insert into user values('test','test이름','1234',now());
 insert into blog values('', '', 'dhsj8405');
 
 -- 블로그 타이틀 로고추가
-update blog set  title = 'test타이틀', logo = 'doolly.png'
+update blog set  title = 'test타이틀', logo = '/images/dolly.png'
 where blog.id = 'dhsj8405';
 
 -- id 중복체크
@@ -35,7 +56,9 @@ from user u
 where u.id = 'dhsj8405'  
 and u.password = '1234';
 
-
+-- 카테고리 추가
+insert into category values(null, '카테고리1', '본문1','dhsj8405') ;
+insert into category values(null, '카테고리2', '본문2','dhsj8405') ;
 
 
 
