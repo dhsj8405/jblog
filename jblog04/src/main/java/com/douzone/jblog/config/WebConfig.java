4a4baca7add7017fd6004c718117faf3be07c954
dhsj8405 <dhsj8405@naver.com>
2021-11-14 01:26:08 +0900

    jblog04 복구(applicationcontext.xml만 없앤상태)

--- jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
+++ jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
@@ -2,15 +2,28 @@ package com.douzone.jblog.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import com.douzone.config.web.FileUploadConfig;
+import com.douzone.config.web.MvcConfig;
+import com.douzone.config.web.SecurityConfig;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan({ "com.douzone.jblog.controller", "com.douzone.jblog.exception" })
-@Import({   FileUploadConfig.class })
+@Import({ MvcConfig.class, SecurityConfig.class })
 public class WebConfig extends WebMvcConfigurerAdapter {
+	// SiteInterceptors
+//	@Bean
+//	public HandlerInterceptor siteInterceptor() {
+//		return new SiteInterceptor();
+//	}
 
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		registry.addInterceptor(siteInterceptor()).addPathPatterns("/**");
+//
+//	}
 
 }
